ca7d9db2b041d829c7db8025aefc134a42f02c9a
Emmalyn Ramos <elr9@njit.edu>
2017-10-19 16:39:12 -0400

    Fixed commenting

--- WK7_HW.php
+++ WK7_HW.php
@@ -17,19 +17,9 @@ try {
 	    $q->execute();
 	    $results = $q->fetchAll();
 
-	    // $sql2 = "SELECT count(id) FROM accounts WHERE id < 6"; 
-	    // $q = $conn->prepare($sql2);
-	    // $q->execute();
-	    // $results = $q->fetchAll();
-
-	    // $id = 1;
-	    // $sql = "select id,email from accounts where id = '$id' ";
-	    // $q = $conn->prepare($sql);
-	    // $q->execute();
-
 	    if($q->rowCount()){
 	    	$rows = count($results);
-	    	echo "Number of records displayed: $rows &lt;br> <br><br>";
+	    	echo "Number of records displayed: $rows <br><br>";
 
 	    	echo "<table border=\"1\">
 	    			<tr>
@@ -61,30 +51,11 @@ try {
 	    } 
 
 
-
-
 	}//END TRY BLOCK
 
 
-// 	    $date = date('Y-m-d', time());
-// 	    $sql ="INSERT INTO accounts (email,fname,lname,phone,birthday,gender,password)
-//     	VALUES ('john@example.com','John', 'Doe',911,'$date','M','1234')";
-//     	$q = $conn->prepare($sql);
-//     	$q->execute();
-// 	    $sql = "select id,email from accounts";
-// 	    $q = $conn->prepare($sql);
-// 	    $q->execute();
-// 	    $results = $q->fetchAll();
-// 	    if($q->rowCount()){
-// 	    	echo "<table border=\"1\"><tr><th>ID</th><th>Email</th></tr>";
-// 	    	foreach ($results as $row) {
-//         		echo "<tr><td>".$row["id"]."</td><td>".$row["email"]."</td></tr>";
-//     		}
-// 	    }else{
-// 	    	echo '0 results';
-// 	    }
-//     }
-catch(PDOException $e){
+catch(PDOException $e)
+	{
     	echo "Connection failed: " . $e->getMessage();
     }//END CATCH BLOCK
 
